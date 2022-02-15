4c31c460bdb41a2ed93b5616601da894d892db1a
raphaelramosds <raphael.opensource@gmail.com>
2022-02-15 10:14:03 -0300

    Navegando entre as notícias

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import  Keys
+from selenium.webdriver.common.keys import Keys
+
+# Objetivo: Em uma pesquisa rápida, quantas vezes se mencionou o tópico citado nos últimos x dias?
+topic = "monark nazismo"
+days = 5
+pages = 4
 
 # Ir ao google.com
 browser = webdriver.Chrome()
 browser.get("http://www.google.com/")
 
 # Pesquisar tópico
-topic = "monark nazismo"
-
 search_field = browser.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")
 search_field.send_keys(topic)
 search_field.send_keys(Keys.ENTER)
@@ -16,4 +19,20 @@ search_field.send_keys(Keys.ENTER)
 news_btn = browser.find_element_by_xpath("//*[@id='hdtb-msb']/div[1]/div/div[4]/a")
 news_btn.click()
 
-# Pegar cards das notícias g-card[class=ftSUBd]
+# Recuperar titulos e tempo de cada notícia
+data = []
+
+for i in range(pages):
+
+  # Pegar as informações
+  card_periods = browser.find_elements_by_css_selector("g-card > div > div > a > div > div.iRPxbe > div.ZE0LJd > p > span")
+  card_titles = browser.find_elements_by_css_selector("g-card > div > div > a > div > div.iRPxbe > div.mCBkyc.y355M.JQe2Ld.nDgy9d")
+
+  # Role a página
+  browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+  # Clique no próximo item
+  browser.find_element_by_css_selector("#pnnext > span:nth-child(2)").click()
+
+# Fechar o navegador
+browser.close()
